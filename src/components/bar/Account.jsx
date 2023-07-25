d4e416dc071cedd688976c464d92427e076760c9
laaledesiempre <alejandrabotacura@outlook.com.ar>
2023-07-24 21:19:26 -0300

    Improovements on counter dot

--- src/components/bar/Account.jsx
+++ src/components/bar/Account.jsx
@@ -10,7 +10,7 @@ export const Account = () => {
       <button className="account-button"><Link to="/contact">Contacto</Link></button>
       <button className="account-button"><Link to="/"><BsHouse /></Link></button>
       <button className="account-button search-icon" onClick={() => { document.querySelector(".input-box").classList.toggle("input-on-site") }} ><BsSearch /></button>
-      <button className="account-button cart-icon"><Link to="/cart"><span className='cart-counter-dot'>{count}</span><BsCart3 /></Link></button>
+      <button className="account-button cart-icon"><Link to="/cart">{count > 0 && <span className='cart-counter-dot'>{count}</span>}<BsCart3 /></Link></button>
     </div>
   )
 }
